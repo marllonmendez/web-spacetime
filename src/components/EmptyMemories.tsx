f4957e8eced4194435f984534a605a966a418f64
Mendes <marlon.mendes@gsd.com.br>
2023-05-19 09:21:20 -0300

    change: restructuring of files and user authentication

---
+++ src/components/EmptyMemories.tsx
@@ -0,0 +1,12 @@
+export function EmptyMemories() {
+  return (
+    <div className="flex flex-1 items-center justify-center">
+      <p className="w-[360px] text-center leading-relaxed">
+        Você ainda não registrou nenhuma lembrança, começa{' '}
+        <a href="" className="font-bold hover:text-gray-50">
+          criar agora!
+        </a>
+      </p>
+    </div>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
         sans: 'var(--font-roboto)',
         alt: 'var(--font-bai-jamjuree)',
       },
+
       colors: {
         gray: {
           50: '#eaeaea',
@@ -36,30 +37,6 @@ module.exports = {
           800: '#48307e',
           900: '#372560',
         },
-        orange: {
-          50: '#ffefeb',
-          100: '#ffccc2',
-          200: '#ffb4a4',
-          300: '#ff927b',
-          400: '#ff7d61',
-          500: '#ff5c3a',
-          600: '#e85435',
-          700: '#b54129',
-          800: '#8c3320',
-          900: '#6b2718',
-        },
-        yellow: {
-          50: '#fff9ec',
-          100: '#ffebc4',
-          200: '#ffe2a7',
-          300: '#ffd47f',
-          400: '#ffcc66',
-          500: '#ffbf40',
-          600: '#e8ae3a',
-          700: '#b5882d',
-          800: '#8c6923',
-          900: '#6b501b',
-        },
         green: {
           50: '#e6fbef',
           100: '#b1f1ce',
@@ -73,17 +50,24 @@ module.exports = {
           900: '#025929',
         },
       },
-      blur: {
-        full: '194px'
-      },
+
       backgroundImage: {
         stripes:
-          "linear-gradient(to bottom, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.1) 12.5%, transparent 12.5%, transparent)"
+          'linear-gradient(to bottom, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.1) 12.5%, transparent 12.5%, transparent)',
       },
+
+      fontSize: {
+        '5xl': '2.5rem',
+      },
+
       backgroundSize: {
         stripes: '100% 8px',
       },
+
+      blur: {
+        full: '194px',
+      },
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 }
